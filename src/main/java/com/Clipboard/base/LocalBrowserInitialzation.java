1a40e6090c1edb4deea8578f74ce501c495d28b7
16679 <16679@10.0.0.193>
2022-11-13 01:25:25 -0500

    Incorporated changes according to the latest selenium version release

--- src/main/java/com/Clipboard/base/LocalBrowserInitialzation.java
+++ src/main/java/com/Clipboard/base/LocalBrowserInitialzation.java
@@ -1,53 +1,50 @@
 package com.Clipboard.base;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;	
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LocalBrowserInitialzation {
-	
-	public static WebDriver driver=null;
-	public static  WebDriver setup(String url, String browserName){
-		
+
+	public static WebDriver driver = null;
+
+	public static WebDriver setup(String url, String browserName) {
+
 		if (driver == null) {
 			switch (browserName) {
 			case "FireFox":
-				WebDriverManager.firefoxdriver().setup();
+				//WebDriverManager.firefoxdriver().setup();
 				driver = new FirefoxDriver();
 				driver.get(url);
 				break;
 
 			case "Chrome":
-				WebDriverManager.chromedriver().setup();
-				driver = new ChromeDriver();
+				//WebDriverManager.chromedriver().setup();	//No need to use WebDriverManager dependency
+				driver = new ChromeDriver();				//because of the latest changes in Selenium 4.6.0
 				driver.get(url);
-				
-				break;
-			case "IE":
-				WebDriverManager.iedriver().setup();
-				driver = new InternetExplorerDriver();
-				driver.get(url);
-				
 				break;
 				
-					
+			case "Edge":
+				//WebDriverManager.iedriver().setup();
+				driver = new EdgeDriver();
+				driver.get(url);
+				break;
+
 			default:
 				throw new IllegalArgumentException(
 						String.format("%s browser is not valid please provide valid browserName", browserName));
 			}
 		}
+		System.out.println("===maximize");
 		driver.manage().window().maximize();
 		return driver;
 	}
-	
+
 	public static void quitDriver() {
 		if (driver != null) {
 			driver.quit();
-			driver=null;
+			driver = null;
 		}
 	}
 }
-	
-	
-
