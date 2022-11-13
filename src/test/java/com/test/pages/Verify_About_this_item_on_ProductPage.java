889e2abbdde043f6532943c6dc51fcd28fc9f856
16679 <16679@10.0.0.193>
2022-11-13 00:22:30 -0500

    Test case update & result on Chrome

--- src/test/java/com/test/pages/Verify_About_this_item_on_ProductPage.java
+++ src/test/java/com/test/pages/Verify_About_this_item_on_ProductPage.java
@@ -15,15 +15,18 @@ public class Verify_About_this_item_on_ProductPage extends TestBase {
 	public void verify_about_this_item_text() {
 		
 		HomePage homePage= new HomePage(LocalBrowserInitialzation.driver);
+
+		String textToverify = homePage
+        								.click_on_hamburgerMenu()
+						        		.click_on_TvElectronicslink()
+						        		.click_on_Televisionslink()
+						        		.click_on_samsungfilter()
+						        		.click_on_featuredDropdown()
+						        		.click_on_priceHighToLow()
+						        		.click_on_secondSamsungTV()
+						        		.check_aboutThisItem_is_displayed();
         
-		homePage
-				.click_on_hamburgerMenu()
-        		.click_on_TvElectronicslink()
-        		.click_on_Televisionslink();
-        
-		int a =1;
-		assertEquals(a==1, "Test ran");
-	
+		//System.out.println("==textToverify=="+textToverify);
+		assertEquals(textToverify, "About this item");
 	}
-	
 }
